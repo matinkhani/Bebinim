6e4f6cf1bec4248e1e42478cf25c4a15e1d12dc2
Matin <mtinkhni@gmail.com>
2023-09-23 10:21:08 +0330

    Fix StyledComponents Rendering

---
+++ app/Styled Components/StyledLoading.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #291212;
+`;
+
+export const Text = styled.h2`
+  font-family: "IRANSansX";
+  color: #fefefe;
+`;
---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { Container, Text } from "./Styled Components/StyledLoading";
+
+export default function Loading() {
+  return (
+    <Container>
+      <Text>لطفاً منتظر بمانید...</Text>
+    </Container>
+  );
+}
