982ed20b1ce99d651f9b14cf2975afb95d301f23
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-10-02 11:29:41 +0330

    Add placeholders to inputs

--- app/Styled Components/login.ts
+++ app/Styled Components/login.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 export const Container = styled.div`
   width: 100vw;
   height: fit-content;
@@ -10,19 +11,21 @@ export const Container = styled.div`
   background-size: cover;
   background-image: url("./images/Login/background.svg");
 `;
+
 export const MiddleContainer = styled.div`
   height: 570px;
   width: 600px;
   display: flex;
   flex-direction: column;
-  /* background-color: red; */
 `;
+
 export const TopContainer = styled.div`
   height: 15%;
   width: 100%;
   display: flex;
   align-items: center;
 `;
+
 export const SignIn = styled.button`
   height: 48px;
   width: 134px;
@@ -42,6 +45,7 @@ export const SignIn = styled.button`
   cursor: pointer;
   letter-spacing: 0em;
 `;
+
 export const DownContainer = styled.div`
   height: 85%;
   width: 100%;
@@ -52,6 +56,7 @@ export const DownContainer = styled.div`
   align-items: center;
   background: rgba(7, 7, 7, 0.8);
 `;
+
 export const TopText = styled.div`
   height: 40px;
   width: 390px;
@@ -61,6 +66,7 @@ export const TopText = styled.div`
   font-size: 16px;
   font-weight: 700;
 `;
+
 export const InputPlace = styled.div`
   height: 171px;
   width: 100%;
@@ -69,7 +75,56 @@ export const InputPlace = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
+
+export const NumInput = styled.div`
+  height: 56px;
+  width: 390px;
+  border-radius: 8px;
+  background-color: rgba(254, 254, 254, 1);
+  display: flex;
+`;
+
+export const Numberr = styled.div`
+  height: 56px;
+  width: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 25px;
+  color: black;
+  border-radius: 8px;
+  font-family: var(--medium);
+`;
+
+export const Line = styled.div`
+  width: 2px;
+  height: 26px;
+  background-color: black;
+`;
+
 export const Input = styled.input`
+  height: 56px;
+  width: 80%;
+  outline: none;
+  border: none;
+  color: #070707;
+  border-radius: 8px;
+  background: #f9f9f9;
+  display: inline-flex;
+  align-items: center;
+  padding-inline: 15px;
+  font-family: var(--medium);
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 0em;
+
+  &::placeholder {
+    color: #070707;
+  }
+`;
+
+export const InputPass = styled.input`
   height: 56px;
   width: 390px;
   outline: none;
@@ -78,16 +133,30 @@ export const Input = styled.input`
   border-radius: 8px;
   background: #f9f9f9;
   display: inline-flex;
-  padding: 14px 195px 6px 16px;
   align-items: center;
+  padding-inline: 15px;
+  direction: rtl;
+  font-family: var(--medium);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+  letter-spacing: 0em;
+  text-align: right;
+
+  &::placeholder {
+    color: black;
+  }
 `;
+
 export const LoginButton = styled.div`
   height: 120px;
   width: 100%;
   display: flex;
-  align-items: end;
-  justify-content: center;
+  align-items: center;
+  justify-content: end;
+  flex-direction: column;
 `;
+
 export const LoginBtn = styled.button`
   display: flex;
   width: 390px;
@@ -104,7 +173,9 @@ export const LoginBtn = styled.button`
   font-family: var(--medium);
   font-size: 16px;
   font-weight: 700;
+  cursor: pointer;
 `;
+
 export const DownText = styled.div`
   height: 69px;
   width: 100%;
