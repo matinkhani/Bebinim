f1f44b943a988ea90e228e5868cb93ffc50a4ee1
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-10-05 20:14:14 +0330

    Create Dynamic route for imdb's best

---
+++ app/Best-IMDb/array.ts
@@ -0,0 +1,140 @@
+const BestIMDbArr = [
+  // Fight Club
+  {
+    id: 1,
+    url: "./images/imdb/fight-club.png",
+    name: "Fight Club",
+    year: "۱۹۹۹",
+    category: "فیلم",
+    image1: "../images/D-IMDB/fightclub1.png",
+    image2: "../images/D-IMDB/fightclub2.jpg",
+    image3: "../images/D-IMDB/fightclub3.jpg",
+    age: "+۱۶",
+    time: "۲:۱۹:۰۰",
+    rate: "۸۹٪",
+    bgImg: "../images/D-IMDB/fightclub.jpg",
+    description:
+      "شخصیت اصلی فیلم جوان بی‌نامی است که به عنوان راوی شناخته می‌شود. راوی زندگی غیرعادی و عجیبی دارد.",
+  },
+  // The Dark Knight
+  {
+    id: 2,
+    url: "./images/imdb/dark-knight.png",
+    name: "The Dark Knight",
+    year: "۲۰۰۸",
+    category: "فیلم",
+    image1: "../images/D-Exclusive/sedato1.jpg",
+    image2: "../images/D-Exclusive/sedato4.jpg",
+    image3: "../images/D-Exclusive/sedato3.jpg",
+    age: "+۱۶",
+    time: "۴۵:۰۰",
+    rate: "۶۲٪",
+    bgImg: "../images/exclusive/sedato.svg",
+    description:
+      "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
+  },
+  // Breaking Bad
+  {
+    id: 3,
+    url: "./images/imdb/breaking-bad.png",
+    name: "Breaking Bad",
+    year: "۲۰۰۸",
+    category: "سریال",
+    image1: "../images/D-IMDB/breaking-bad1.png",
+    image2: "../images/D-IMDB/breaking-bad2.jpg",
+    image3: "../images/D-IMDB/breaking-bad3.jpg",
+    age: "+۱۶",
+    time: "۴۵:۰۰",
+    rate: "۹۷٪",
+    bgImg: "../images/D-IMDB/breaking-bad.jpg",
+    description:
+      "بریکینگ بد داستان والتر وایت دانشمند شیمی زحمتکش و بی‌حاشیه‌ای را به تصویر می‌کشد که دبیر دبیرستان است. وایت در ابتدای داستان متوجه می‌شود که مبتلا به سرطان ریه است.",
+  },
+  // Inception
+  {
+    id: 4,
+    url: "./images/imdb/inception.png",
+    name: "Inception",
+    year: "۲۰۱۰",
+    category: "فیلم",
+    image1: "../images/D-Exclusive/sedato1.jpg",
+    image2: "../images/D-Exclusive/sedato4.jpg",
+    image3: "../images/D-Exclusive/sedato3.jpg",
+    age: "+۳",
+    time: "۴۵:۰۰",
+    rate: "۶۲٪",
+    bgImg: "../images/exclusive/sedato.svg",
+    description:
+      "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
+  },
+  // La La Land
+  {
+    id: 5,
+    url: "./images/imdb/lala-land.png",
+    name: "La La Land",
+    year: "۲۰۱۶",
+    category: "فیلم",
+    image1: "../images/D-Exclusive/sedato1.jpg",
+    image2: "../images/D-Exclusive/sedato4.jpg",
+    image3: "../images/D-Exclusive/sedato3.jpg",
+    age: "+۳",
+    time: "۴۵:۰۰",
+    rate: "۶۲٪",
+    bgImg: "../images/exclusive/sedato.svg",
+    description:
+      "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
+  },
+  // Coda
+  {
+    id: 6,
+    url: "./images/imdb/coda.png",
+    name: "Coda",
+    year: "۲۰۲۱",
+    category: "فیلم",
+    image1: "../images/D-Exclusive/sedato1.jpg",
+    image2: "../images/D-Exclusive/sedato4.jpg",
+    image3: "../images/D-Exclusive/sedato3.jpg",
+    age: "+۳",
+    time: "۴۵:۰۰",
+    rate: "۶۲٪",
+    bgImg: "../images/exclusive/sedato.svg",
+    description:
+      "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
+  },
+  // Interstellar
+  {
+    id: 7,
+    url: "./images/imdb/interstellar.png",
+    name: "Interstellar",
+    year: "۲۰۱۴",
+    category: "فیلم",
+    image1: "../images/D-Exclusive/sedato1.jpg",
+    image2: "../images/D-Exclusive/sedato4.jpg",
+    image3: "../images/D-Exclusive/sedato3.jpg",
+    age: "+۳",
+    time: "۴۵:۰۰",
+    rate: "۶۲٪",
+    bgImg: "../images/exclusive/sedato.svg",
+    description:
+      "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
+  },
+  // The Godfather
+  {
+    id: 8,
+    url: "./images/imdb/god-father.png",
+    name: "The Godfather",
+    year: "۱۹۷۲",
+    category: "فیلم",
+    image1: "../images/D-Exclusive/sedato1.jpg",
+    image2: "../images/D-Exclusive/sedato4.jpg",
+    image3: "../images/D-Exclusive/sedato3.jpg",
+    age: "+۳",
+    time: "۴۵:۰۰",
+    rate: "۶۲٪",
+    bgImg: "../images/exclusive/sedato.svg",
+    description:
+      "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
+  },
+];
+
+export default BestIMDbArr;
