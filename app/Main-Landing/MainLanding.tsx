43888cf25abc08b661796894bea038366fab09d7
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-10-09 12:56:36 +0330

    Create Main carousel with swiper (bug)

--- app/Main-Landing/MainLanding.tsx
+++ app/Main-Landing/MainLanding.tsx
@@ -21,89 +21,106 @@ import {
 } from "../Styled Components/MainLanding";
 import MainMovie from "./array";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./main.css";
+import { Navigation } from "swiper/modules";
 
 export default function MainLanding() {
   return (
     <>
-      {MainMovie.map((item, index) => {
-        return (
-          <Container style={{ backgroundImage: `url(${item.url})` }}>
-            <FilmPoster>
-              <LeftSide>
-                <ArrowIconsPlace>
-                  <ArrowIcons src="./images/arrowcircleleft.svg" />
-                  <ArrowIcons src="./images/arrowcircleright.svg" />
-                </ArrowIconsPlace>
-              </LeftSide>
+      <Swiper
+        navigation={true}
+        slidesPerView={1}
+        speed={1200}
+        modules={[Navigation]}
+        className="mySwiper"
+        slidesPerGroup={1}
+        loop={true}
+      >
+        {MainMovie.map((item, index) => {
+          return (
+            <SwiperSlide>
+              <Container style={{ backgroundImage: `url(${item.url})` }}>
+                <FilmPoster>
+                  <LeftSide>
+                    <ArrowIconsPlace>
+                      <ArrowIcons src="./images/arrowcircleleft.svg" />
+                      <ArrowIcons src="./images/arrowcircleright.svg" />
+                    </ArrowIconsPlace>
+                  </LeftSide>
 
-              <RightSide>
-                <ExplainContainer>
-                  <ExplainAndWatch>
-                    <MoreExplainText
-                      style={
-                        item.id === 1
-                          ? { color: "black" }
-                          : item.id === 2
-                          ? { color: "black" }
-                          : item.id === 3
-                          ? { color: "white" }
-                          : item.id === 4
-                          ? { color: "white" }
-                          : {}
-                      }
-                    >
-                      توضیحات بیشتر
-                    </MoreExplainText>
-                    <Link href={`Main-Landing/${item.id}`}>
-                      <MoreIcon src="./images/mores.svg" />
-                    </Link>
-                    <Link href={`Main-Landing/${item.id}`}>
-                      <MoreExplainBtn>
-                        تماشا کنید
-                        <WatchSVG src="./images/watch.svg" />
-                      </MoreExplainBtn>
-                    </Link>
-                  </ExplainAndWatch>
-                  <Text
-                    style={
-                      item.id === 1
-                        ? { color: "black" }
-                        : item.id === 2
-                        ? { color: "black" }
-                        : item.id === 3
-                        ? { color: "white" }
-                        : item.id === 4
-                        ? { color: "white" }
-                        : {}
-                    }
-                  >
-                    {item.text}
-                  </Text>
-                </ExplainContainer>
-              </RightSide>
-            </FilmPoster>
+                  <RightSide>
+                    <ExplainContainer>
+                      <ExplainAndWatch>
+                        <MoreExplainText
+                          style={
+                            item.id === 1
+                              ? { color: "black" }
+                              : item.id === 2
+                              ? { color: "black" }
+                              : item.id === 3
+                              ? { color: "white" }
+                              : item.id === 4
+                              ? { color: "white" }
+                              : {}
+                          }
+                        >
+                          توضیحات بیشتر
+                        </MoreExplainText>
+                        <Link href={`Main-Landing/${item.id}`}>
+                          <MoreIcon src="./images/mores.svg" />
+                        </Link>
+                        <Link href={`Main-Landing/${item.id}`}>
+                          <MoreExplainBtn>
+                            تماشا کنید
+                            <WatchSVG src="./images/watch.svg" />
+                          </MoreExplainBtn>
+                        </Link>
+                      </ExplainAndWatch>
+                      <Text
+                        style={
+                          item.id === 1
+                            ? { color: "black" }
+                            : item.id === 2
+                            ? { color: "black" }
+                            : item.id === 3
+                            ? { color: "white" }
+                            : item.id === 4
+                            ? { color: "white" }
+                            : {}
+                        }
+                      >
+                        {item.text}
+                      </Text>
+                    </ExplainContainer>
+                  </RightSide>
+                </FilmPoster>
 
-            <BebinimBox>
-              <LinksPlace>
-                <BebinimLinks>
-                  <FreshText>انیمیشن تازه ببینیم</FreshText>
-                  <FreshSVG src="./images/freshAnimation.svg" />
-                </BebinimLinks>
+                <BebinimBox>
+                  <LinksPlace>
+                    <BebinimLinks>
+                      <FreshText>انیمیشن تازه ببینیم</FreshText>
+                      <FreshSVG src="./images/freshAnimation.svg" />
+                    </BebinimLinks>
 
-                <BebinimLinks>
-                  <FreshText>سریال تازه ببینیم</FreshText>
-                  <FreshSVG src="./images/freshSerial.svg" />
-                </BebinimLinks>
+                    <BebinimLinks>
+                      <FreshText>سریال تازه ببینیم</FreshText>
+                      <FreshSVG src="./images/freshSerial.svg" />
+                    </BebinimLinks>
 
-                <BebinimLinks>
-                  <FreshText>فیلم تازه ببینیم</FreshText>
-                  <FreshSVG src="./images/freshMovie.svg" />
-                </BebinimLinks>
-              </LinksPlace>
-            </BebinimBox>
-          </Container>
-        );
-      })}
+                    <BebinimLinks>
+                      <FreshText>فیلم تازه ببینیم</FreshText>
+                      <FreshSVG src="./images/freshMovie.svg" />
+                    </BebinimLinks>
+                  </LinksPlace>
+                </BebinimBox>
+              </Container>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </>
   );
 }
