74d7e51276639be7f11e3a71027d9ed06d842bd7
Mehdi <mhdy8017@gmail.com>
2023-10-11 15:49:27 -0700

    Set Carousel to Main Landing

--- app/Main-Landing/MainLanding.tsx
+++ app/Main-Landing/MainLanding.tsx
@@ -10,7 +10,6 @@ import {
   LeftSide,
   RightSide,
   ArrowIconsPlace,
-  ArrowIcons,
   ExplainContainer,
   ExplainAndWatch,
   Text,
@@ -18,73 +17,49 @@ import {
   MoreExplainBtn,
   WatchSVG,
   MoreIcon,
+  ArrowIconsRight,
+  ArrowIconsLeft,
 } from "../Styled Components/MainLanding";
 import MainMovie from "./array";
 import Link from "next/link";
+import Carousel from "nuka-carousel";
 import "./styles.css";
-import { useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
 
+const renderCenterLeftControls = ({
+  previousSlide,
+}: {
+  previousSlide: any;
+}) => (
+  <ArrowIconsLeft onClick={previousSlide} src="./images/arrowcircleleft.svg" />
+);
+
+const renderCenterRightControls = ({ nextSlide }: { nextSlide: any }) => {
+  return (
+    <ArrowIconsRight onClick={nextSlide} src="./images/arrowcircleRight.svg" />
+  );
+};
 export default function MainLanding() {
-  const [currentSlide, setCurrentSlide] = useState<number>(0);
-  const [loaded, setLoaded] = useState<boolean>(false);
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
-    },
-    created() {
-      setLoaded(true);
-    },
-  });
 
   return (
     <>
-      <div className="navigation-wrapper">
-        <div ref={sliderRef} className="keen-slider">
-          {MainMovie.map((item, index) => {
-            return (
-              <Container
-                className="keen-slider__slide"
-                style={{ backgroundImage: `url(${item.url})` }}
-              >
-                <FilmPoster>
-                  <LeftSide>
-                    <ArrowIconsPlace>
-                      <ArrowIcons src="./images/arrowcircleleft.svg" />
-                      <ArrowIcons src="./images/arrowcircleright.svg" />
-                    </ArrowIconsPlace>
-                  </LeftSide>
+      <Carousel
+        disableEdgeSwiping={false}
+        renderCenterLeftControls={renderCenterLeftControls}
+        renderCenterRightControls={renderCenterRightControls}
+        wrapAround={true}
+      >
+        {MainMovie.map((item, index) => {
+          return (
+            <Container style={{ backgroundImage: `url(${item.url})` }}>
+              <FilmPoster>
+                <LeftSide>
+                  <ArrowIconsPlace></ArrowIconsPlace>
+                </LeftSide>
 
-                  <RightSide>
-                    <ExplainContainer>
-                      <ExplainAndWatch>
-                        <MoreExplainText
-                          style={
-                            item.id === 1
-                              ? { color: "black" }
-                              : item.id === 2
-                              ? { color: "black" }
-                              : item.id === 3
-                              ? { color: "white" }
-                              : item.id === 4
-                              ? { color: "white" }
-                              : {}
-                          }
-                        >
-                          توضیحات بیشتر
-                        </MoreExplainText>
-                        <Link href={`Main-Landing/${item.id}`}>
-                          <MoreIcon src="./images/mores.svg" />
-                        </Link>
-                        <Link href={`Main-Landing/${item.id}`}>
-                          <MoreExplainBtn>
-                            تماشا کنید
-                            <WatchSVG src="./images/watch.svg" />
-                          </MoreExplainBtn>
-                        </Link>
-                      </ExplainAndWatch>
-                      <Text
+                <RightSide>
+                  <ExplainContainer>
+                    <ExplainAndWatch>
+                      <MoreExplainText
                         style={
                           item.id === 1
                             ? { color: "black" }
@@ -97,81 +72,59 @@ export default function MainLanding() {
                             : {}
                         }
                       >
-                        {item.text}
-                      </Text>
-                    </ExplainContainer>
-                  </RightSide>
-                </FilmPoster>
+                        توضیحات بیشتر
+                      </MoreExplainText>
+                      <Link href={`Main-Landing/${item.id}`}>
+                        <MoreIcon src="./images/mores.svg" />
+                      </Link>
+                      <Link href={`Main-Landing/${item.id}`}>
+                        <MoreExplainBtn>
+                          تماشا کنید
+                          <WatchSVG src="./images/watch.svg" />
+                        </MoreExplainBtn>
+                      </Link>
+                    </ExplainAndWatch>
+                    <Text
+                      style={
+                        item.id === 1
+                          ? { color: "black" }
+                          : item.id === 2
+                          ? { color: "black" }
+                          : item.id === 3
+                          ? { color: "white" }
+                          : item.id === 4
+                          ? { color: "white" }
+                          : {}
+                      }
+                    >
+                      {item.text}
+                    </Text>
+                  </ExplainContainer>
+                </RightSide>
+              </FilmPoster>
 
-                <BebinimBox>
-                  <LinksPlace>
-                    <BebinimLinks>
-                      <FreshText>انیمیشن تازه ببینیم</FreshText>
-                      <FreshSVG src="./images/freshAnimation.svg" />
-                    </BebinimLinks>
+              <BebinimBox>
+                <LinksPlace>
+                  <BebinimLinks>
+                    <FreshText>انیمیشن تازه ببینیم</FreshText>
+                    <FreshSVG src="./images/freshAnimation.svg" />
+                  </BebinimLinks>
 
-                    <BebinimLinks>
-                      <FreshText>سریال تازه ببینیم</FreshText>
-                      <FreshSVG src="./images/freshSerial.svg" />
-                    </BebinimLinks>
+                  <BebinimLinks>
+                    <FreshText>سریال تازه ببینیم</FreshText>
+                    <FreshSVG src="./images/freshSerial.svg" />
+                  </BebinimLinks>
 
-                    <BebinimLinks>
-                      <FreshText>فیلم تازه ببینیم</FreshText>
-                      <FreshSVG src="./images/freshMovie.svg" />
-                    </BebinimLinks>
-                  </LinksPlace>
-                </BebinimBox>
-              </Container>
-            );
-          })}
-        </div>
-        {loaded && instanceRef.current && (
-          <>
-            <Arrow
-              left
-              onClick={(e: any) =>
-                e.stopPropagation() || instanceRef.current?.prev()
-              }
-              disabled={currentSlide === 0}
-            />
-
-            <Arrow
-              onClick={(e: any) =>
-                e.stopPropagation() || instanceRef.current?.next()
-              }
-              disabled={
-                currentSlide ===
-                instanceRef.current.track.details.slides.length - 1
-              }
-            />
-          </>
-        )}
-      </div>
+                  <BebinimLinks>
+                    <FreshText>فیلم تازه ببینیم</FreshText>
+                    <FreshSVG src="./images/freshMovie.svg" />
+                  </BebinimLinks>
+                </LinksPlace>
+              </BebinimBox>
+            </Container>
+          );
+        })}
+      </Carousel>
     </>
-  );
-}
-
-function Arrow(props: {
-  disabled: boolean;
-  left?: boolean;
-  onClick: (e: any) => void;
-}) {
-  const disabeld = props.disabled ? " arrow--disabled" : "";
-  return (
-    <svg
-      onClick={props.onClick}
-      className={`arrow ${
-        props.left ? "arrow--left" : "arrow--right"
-      } ${disabeld}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-      )}
-    </svg>
   );
 }
