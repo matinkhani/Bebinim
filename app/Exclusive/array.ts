8a2b2b34d34cd5a88d5f218f1066bf52819c8246
Mehdi <mhdy8017@gmail.com>
2023-10-14 11:12:38 -0700

    Create Saved List for Exclusive

--- app/Exclusive/array.ts
+++ app/Exclusive/array.ts
@@ -16,7 +16,9 @@ const ExclusiveArr = [
     bgImg: "../images/exclusive/sedato.svg",
     description:
       "«صداتو» یک مسابقه موزیکال-معمایی به کارگردانی و تهیه‌کنندگی حامد جوادزاده و با اجرای محسن کیایی محصول سال ۱۴۰۲ می‌باشد که برای پلتفرم نمایش‌خوانگی فیلیمو ساخته شد.",
-  },
+    save: "../images/Watch/save.svg",
+    saved: "../images/Watch/saved.svg",
+    },
   // Actor
   {
     id: 2,
@@ -34,7 +36,9 @@ const ExclusiveArr = [
     bgImg: "../images/D-Exclusive/actor.jpg",
     description:
       "داستان این سریال، درباره زندگی پر فراز و نشیب چند بازیگر مستعد تئاتر است که بعد از پیشنهادی که به آن‌ها می‌شود زندگی‌ شان درگیر ماجراهایی پیچیده می‌شود.",
-  },
+    save: "../images/Watch/save.svg",
+    saved: "../images/Watch/saved.svg",
+    },
   // The God Father
   {
     id: 3,
@@ -52,7 +56,9 @@ const ExclusiveArr = [
     bgImg: "../images/D-Exclusive/pedar.jpg",
     description:
       "یک مجموعه نمایش خانگی ایرانی به کارگردانی سعید ابوطالب و محمدرضا رضائیان است که بر اساس بازی مافیا ساخته شده‌است.",
-  },
+    save: "../images/Watch/save.svg",
+    saved: "../images/Watch/saved.svg",
+    },
   // Nato
   {
     id: 4,
@@ -70,7 +76,9 @@ const ExclusiveArr = [
     bgImg: "../images/D-Exclusive/nato.jpg",
     description:
       "ناتو سریال، برنامه، مسابقه یا رئالیتی شو جدید نمایش خانگی است که جایگزین فصل اول پدرخوانده است. سریال یا برنامه ناتو به صورت هفتگی برای دانلود قرار می گیرد.",
-  },
+    save: "../images/Watch/save.svg",
+    saved: "../images/Watch/saved.svg",
+    },
 ];
 
 export default ExclusiveArr;
