9c7f9dde73487f9bb3634dabf5e29091e1d88c4b
Mehdi <mhdy8017@gmail.com>
2023-10-16 16:30:11 -0700

    Edit Section for Details

--- app/Styled Components/ZakhmKari.ts
+++ app/Styled Components/ZakhmKari.ts
@@ -6,14 +6,138 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #291212;
+`;
+
+export const BgImg = styled.div`
+  height: 100%;
+  width: 115%;
+  background-image: url("./images/ZakhmKari/background.svg");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const MovieMain = styled.div`
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(
+    180deg,
+    #291212 0%,
+    rgba(46, 8, 8, 0.45) 18.35%,
+    rgba(51, 0, 0, 0) 78.96%,
+    rgba(43, 15, 15, 0.82) 99.99%,
+    #291212 100%
+  );
+`;
+
+export const Details = styled.div`
+  height: 300px;
+  width: 87.5%;
+  gap: 280px;
+  display: flex;
+  justify-content: end;
+`;
+export const MovieIacon = styled.div`
+  width: 276px;
+  height: 181px;
+`;
+
+export const DetailsText = styled.div`
+  height: 100%;
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+`;
+export const Director = styled.div`
+  height: 45px;
+  width: fit-content;
+  display: flex;
+  align-items: end;
+  color: #757575;
+  font-family: var(--medium);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+export const RatePlace = styled.div`
+  height: 55px;
+  width: 30%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 24px;
+`;
+export const Category1 = styled.div`
+  height: 20px;
+  width: 28px;
+  padding: 4px;
+  gap: 10px;
+  border-radius: 8px;
+  background: #6d2a38;
+  color: var(--F9F9F9, #f9f9f9);
+  text-align: center;
+  font-family: var(--medium);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 12px;
+`;
+export const Category2 = styled.div`
+  height: 20px;
+  width: 51px;
+  padding: 4px;
+  gap: 10px;
+  border-radius: 8px;
+  background: #6d2a38;
+  color: var(--F9F9F9, #f9f9f9);
+  text-align: center;
+  font-family: var(--medium);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 12px;
+`;
+export const Rate = styled.div`
+  height: 21px;
+  width: 44px;
+  gap: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #b55253;
+  text-align: center;
+  font-family: var(--medium);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const DetailsMovieText = styled.div`
+height: 72px;
+width: 789px;
+direction: rtl;
+color: #CBCBCB;
+text-align: right;
+font-family: var(--medium);
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+`
+
+export const MovieCarousel = styled.div`
+  height: 160px;
+  width: 100%;
 `;
 
 export const TextPlace = styled.div`
   height: 10%;
-  width: 28.4%;
+  width: 27.8%;
   display: flex;
   justify-content: center;
+  align-self: end;
   align-items: end;
 `;
 
@@ -24,11 +148,3 @@ export const Text = styled.p`
   font-weight: 700;
   color: rgba(254, 254, 254, 1);
 `;
-
-export const CarouselContainer = styled.div`
-height: 450px;
-width: 115%;
-margin-top: 27px;
-background-image: url("./images/ZakhmKari/background.svg");
-background-size: cover;
-`
--- app/ZakhmKari/ZakhmKari.tsx
+++ app/ZakhmKari/ZakhmKari.tsx
@@ -1,16 +1,60 @@
-import React from 'react'
-import { CarouselContainer, Container, Text, TextPlace } from '../Styled Components/ZakhmKari'
+import React from "react";
+import {
+  BgImg,
+  Category1,
+  Category2,
+  // CarouselContainer,
+  Container,
+  Details,
+  DetailsMovieText,
+  DetailsText,
+  Director,
+  MovieCarousel,
+  MovieIacon,
+  MovieMain,
+  Rate,
+  RatePlace,
+  Text,
+  TextPlace,
+} from "../Styled Components/ZakhmKari";
 
 export default function Movie() {
   return (
     <Container>
-        <TextPlace>
-            <Text>
-            سریال زخم کاری: بازگشت
-            </Text>
-        </TextPlace>
-
-<CarouselContainer></CarouselContainer>
+      <TextPlace>
+        <Text>سریال زخم کاری: بازگشت</Text>
+      </TextPlace>
+      <BgImg>
+        <MovieMain>
+          <Details>
+            <MovieIacon>
+              <img src="./images/ZakhmKari/mov_logo.svg" />
+            </MovieIacon>
+            <DetailsText>
+              <Director>کارگردان: محمدحسین مهدویان</Director>
+              <RatePlace>
+                <Rate>
+                  ۳۰٪
+                  <img src="./images/ZakhmKari/rate.svg" />
+                </Rate>
+                <Category2>خانوادگی</Category2>
+                <Category1>درام</Category1>
+              </RatePlace>
+              <DetailsMovieText>
+                در نبود مالک حالا مدعیان تاج و تخت او یکی یکی از راه می‌رسند و
+                این آغاز آشوب دیگری است… هنوز بوی خون به مشام می‌رسد!» به دلیل
+                حفظ جذابیت‌ها، داستان کامل فصل دوم زخم کاری اعلام نمی‌شود اما
+                اگر بخواهیم با توجه به پایان‌بندی فصل اول کمی تجسم کنیم و فصل
+                دوم را در ذهنمان طراحی کنیم باید منتظر زنده ماندن مالک از ضربات
+                چاقو، خروش و انتقام ...
+              </DetailsMovieText>
+            </DetailsText>
+          </Details>
+          <MovieCarousel>
+            
+          </MovieCarousel>
+        </MovieMain>
+      </BgImg>
     </Container>
-  )
+  );
 }
