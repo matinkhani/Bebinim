6840c7c64dbc5838ed21a8a58f88fe580b9c6a15
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-10-28 14:35:02 +0330

    Setup redux-persist to project

--- app/Redux/createslice.tsx
+++ app/Redux/createslice.tsx
@@ -31,77 +31,17 @@ interface initial_state {
   SavedList: SavedTypes[];
 }
 
-// Value of CheckLogin state for localstorage
-const LoginState =
-  localStorage.getItem("login") !== null
-    ? JSON.parse(localStorage.getItem("login") || "")
-    : false;
-
-// Value of OneMonthAuto state for localstorage
-const OneAutoState =
-  localStorage.getItem("oneAuto") !== null
-    ? JSON.parse(localStorage.getItem("oneAuto") || "")
-    : false;
-
-// Value of OneMonth state for localstorage
-const OneMonthState =
-  localStorage.getItem("oneMonth") !== null
-    ? JSON.parse(localStorage.getItem("oneMonth") || "")
-    : false;
-
-// Value of ThreeMonth state for localstorage
-const ThreeMonthState =
-  localStorage.getItem("threeMonth") !== null
-    ? JSON.parse(localStorage.getItem("threeMonth") || "")
-    : false;
-
-// Value of SixMonth state for localstorage
-const SixMonthState =
-  localStorage.getItem("sixMonth") !== null
-    ? JSON.parse(localStorage.getItem("sixMonth") || "")
-    : false;
-
-// Value of SaveList state for localstorage
-const SaveListState =
-  localStorage.getItem("savelist") !== null
-    ? JSON.parse(localStorage.getItem("savelist") || "")
-    : [];
-
-// Value of Name state for localstorage
-const NameState =
-  localStorage.getItem("name") !== null
-    ? JSON.parse(localStorage.getItem("name") || "")
-    : "";
-
-// Value of Email state for localstorage
-const EmailState =
-  localStorage.getItem("email") !== null
-    ? JSON.parse(localStorage.getItem("email") || "")
-    : "";
-
-// Value of Phone state for localstorage
-const PhoneState =
-  localStorage.getItem("phone") !== null
-    ? JSON.parse(localStorage.getItem("phone") || "")
-    : "";
-
-// Value of ShowPhoneNumber state for localstorage
-const NumberState =
-  localStorage.getItem("number") !== null
-    ? JSON.parse(localStorage.getItem("number") || "")
-    : "";
-
 const initialState: initial_state = {
-  PhoneNumber: PhoneState,
-  Name: NameState,
-  Email: EmailState,
-  CheckLogin: LoginState,
-  OneMonthAuto: OneAutoState,
-  OneMonth: OneMonthState,
-  ThreeMonth: ThreeMonthState,
-  SixMonth: SixMonthState,
-  SavedList: SaveListState,
-  ShowPhoneNumber: NumberState,
+  PhoneNumber: '',
+  Name: '',
+  Email: '',
+  CheckLogin: false,
+  OneMonthAuto: false,
+  OneMonth: false,
+  ThreeMonth: false,
+  SixMonth: false,
+  SavedList: [],
+  ShowPhoneNumber: '',
 };
 
 export const bebinimReducers = createSlice({
@@ -110,50 +50,39 @@ export const bebinimReducers = createSlice({
   reducers: {
     GetNumber: (state, { payload }: { payload: string }) => {
       state.PhoneNumber = payload;
-      localStorage.setItem("phone", JSON.stringify(state.PhoneNumber));
     },
     ShowNumber: (state, { payload }: { payload: string }) => {
       state.ShowPhoneNumber = payload;
-      localStorage.setItem("number", JSON.stringify(state.ShowPhoneNumber));
     },
     GetName: (state, { payload }: { payload: string }) => {
       state.Name = payload;
-      localStorage.setItem("name", JSON.stringify(state.Name));
     },
     GetEmail: (state, { payload }: { payload: string }) => {
       state.Email = payload;
-      localStorage.setItem("email", JSON.stringify(state.Email));
     },
     CreateAccount: (state, { payload }: { payload: boolean }) => {
       state.CheckLogin = payload;
-      localStorage.setItem("login", JSON.stringify(state.CheckLogin));
     },
     ChangeOneMonthAuto: (state, { payload }: { payload: boolean }) => {
       state.OneMonthAuto = payload;
-      localStorage.setItem("oneAuto", JSON.stringify(state.OneMonthAuto));
     },
     ChangeOneMonth: (state, { payload }: { payload: boolean }) => {
       state.OneMonth = payload;
-      localStorage.setItem("oneMonth", JSON.stringify(state.OneMonth));
     },
     ChangeThreeMonth: (state, { payload }: { payload: boolean }) => {
       state.ThreeMonth = payload;
-      localStorage.setItem("threeMonth", JSON.stringify(state.ThreeMonth));
     },
     ChangeSixMonth: (state, { payload }: { payload: boolean }) => {
       state.SixMonth = payload;
-      localStorage.setItem("sixMonth", JSON.stringify(state.SixMonth));
     },
     AddToSavedList: (state, { payload }: { payload: SavedTypes }) => {
       let savedItem = [...state.SavedList];
       savedItem.push(payload);
       state.SavedList = savedItem;
-      localStorage.setItem("savelist", JSON.stringify(state.SavedList));
     },
     DeleteItem: (state, { payload }: { payload: any }) => {
       let removedList = state.SavedList.filter((elem) => elem.id !== payload);
       state.SavedList = removedList;
-      localStorage.setItem("savelist", JSON.stringify(state.SavedList));
     },
   },
 });
--- app/Redux/store.tsx
+++ app/Redux/store.tsx
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  bebinimReducers  from "./createslice";
+import bebinimReducers from "./createslice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+
+const reducers = combineReducers({
+    Reducer: bebinimReducers,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const storee = configureStore({
-    reducer:{
-       Reducer : bebinimReducers
-    }
-})
-export type RoutState = ReturnType<typeof storee.getState>
-export default storee
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+export type RoutState = ReturnType<typeof storee.getState>;
+export default storee;
