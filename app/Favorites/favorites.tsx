943b8c3c6f08060059c2aec7c660f404d5150122
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-11-03 16:17:56 +0330

    Responsive favorites until 1200px

--- app/Favorites/favorites.tsx
+++ app/Favorites/favorites.tsx
@@ -42,6 +42,7 @@ export default function Favorites() {
   };
   const Responsive600 = useMediaQuery("(max-width:600px)");
   const Responsive900 = useMediaQuery("(max-width:900px)");
+  const Responsive1200 = useMediaQuery("(max-width:1200px)");
   return (
     <Container>
       <TextPlace>
@@ -109,6 +110,57 @@ export default function Favorites() {
               style={{
                 height: "100%",
                 width: "88%",
+                gap: "5px",
+                marginLeft: "40px",
+                marginTop: "40px",
+              }}
+              renderCenterLeftControls={renderCenterLeftControls}
+              renderCenterRightControls={renderCenterRightControls}
+            >
+              {FavoritesArr.map((item, index) => (
+                <div
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <FavoriteImg key={index} src={item.url} />
+                  <Hover>
+                    {" "}
+                    <Link href={`Favorites/${item.id}`}>
+                      <LinkPlace>
+                        <HoverText>
+                          <Details>
+                            <NameFilm>{item.name}</NameFilm>
+                            <DateFilm>
+                              <Year>{item.year}</Year>
+                              <Line />
+                              <Name>{item.category}</Name>
+                            </DateFilm>
+                          </Details>
+                        </HoverText>
+                      </LinkPlace>
+                    </Link>
+                  </Hover>
+                </div>
+              ))}
+            </Carousel>
+          </ArrowPlace>
+        </>
+      ) : Responsive1200 ? (
+        <>
+          <ArrowPlace>
+            <Carousel
+              slidesToScroll={2}
+              slidesToShow={4}
+              cellSpacing={0}
+              wrapAround={true}
+              style={{
+                height: "100%",
+                width: "92%",
                 gap: "5px",
                 marginLeft: "40px",
                 marginTop: "40px",
--- app/Styled Components/favorites.ts
+++ app/Styled Components/favorites.ts
@@ -20,7 +20,7 @@ export const TextPlace = styled.div`
   justify-content: center;
   align-items: end;
 
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 1200px) {
     width: 100%;
     height: 25%;
   }
@@ -143,7 +143,7 @@ export const Hover = styled.div`
     display: flex;
   }
 
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 1200px) {
     height: 180px;
     width: 140px;
   }
@@ -157,7 +157,7 @@ export const FavoriteImg = styled.img`
   cursor: pointer;
   position: relative;
 
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 1200px) {
     height: 180px;
     width: 140px;
   }
@@ -170,7 +170,7 @@ export const LinkPlace = styled.div`
   justify-content: center;
   align-items: center;
 
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 1200px) {
     height: 180px;
     width: 140px;
   }
