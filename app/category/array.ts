ede41f9b0b98d4e5e66aaeea48c712def49de8a1
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-11-07 03:07:29 +0330

    Edit category section and change images

---
+++ app/category/array.ts
@@ -0,0 +1,74 @@
+const CategoryArray = [
+  //1
+  {
+    src: "/./images/Category/serial.png",
+    alt: "serial tab",
+  },
+  //2
+  {
+    src: "/./images/Category/ekhtesasi.png",
+    alt: "exclusive tab",
+  },
+  //3
+  {
+    src: "/./images/Category/online.png",
+    alt: "cinema online tab",
+  },
+  //4
+  {
+    src: "/./images/Category/irani.png",
+    alt: "irani tab",
+  },
+  //5
+  {
+    src: "/./images/Category/imdb.png",
+    alt: "best of imdb tab",
+  },
+  //6
+  {
+    src: "/./images/Category/rielitisho.png",
+    alt: "reality show tab",
+  },
+  //7
+  {
+    src: "/./images/Category/taksho.png",
+    alt: "talk show tab",
+  },
+  //8
+  {
+    src: "/./images/Category/animat.png",
+    alt: "animation tab",
+  },
+  //9
+  {
+    src: "/./images/Category/khareji.png",
+    alt: "film tab",
+  },
+  //10
+  {
+    src: "/./images/Category/torki.png",
+    alt: "turkey movie tab",
+  },
+  //11
+  {
+    src: "/./images/Category/koreii.png",
+    alt: "korean movie tab",
+  },
+  //12
+  {
+    src: "/./images/Category/hendi.png",
+    alt: "indian movie tab",
+  },
+  //13
+  {
+    src: "/./images/Category/mostanad.png",
+    alt: "mostanad tab",
+  },
+  //14
+  {
+    src: "/./images/Category/kotah.png",
+    alt: "short movie tab",
+  },
+];
+
+export default CategoryArray;
--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,33 +1,19 @@
 "use client";
-import {
-  CategoryItems,
-  Container,
-  Image1,
-  Image10,
-  Image11,
-  Image12,
-  Image13,
-  Image14,
-  Image15,
-  Image2,
-  Image3,
-  Image4,
-  Image5,
-  Image6,
-  Image7,
-  Image8,
-  Image9,
-  ImagesPlace,
-  Null,
-} from "@/app/Styled Components/category";
+import { Container, ImagesPlace, Null } from "@/app/Styled Components/category";
 import React from "react";
 import { GlobalStyle } from "../Styled Components/Landing";
 import Header from "../Header/Header";
 import "animate.css";
 import Link from "next/link";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Image from "next/image";
+import CategoryArray from "./array";
 
 export default function Category() {
+  const Responsive480 = useMediaQuery("(max-width:480px)");
+  const Responsive800 = useMediaQuery("(max-width:800px)");
+  const Responsive1240 = useMediaQuery("(max-width:1240px)");
+
   return (
     <React.Fragment>
       <GlobalStyle />
@@ -35,22 +21,68 @@ export default function Category() {
         <Header />
         <Null />
         <ImagesPlace className="animate__animated animate__fadeInUpBig">
-          <Image1 />
-          <Image2 />
-          <Image3 />
-          <Image4 />
-          <Image5 />
-          <Image6 />
-          <Image7 />
-          <Image8 />
-          <Image9 />
-          <Image10 />
-          <Image11 />
-          <Image12 />
-          <Image14 />
-          <Image15 />
+          {CategoryArray.map((item, index) => {
+            return (
+              <>
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  height={200}
+                  width={288}
+                  style={
+                    Responsive480
+                      ? {
+                          height: 150,
+                          width: 250,
+                          backgroundPosition: "80% 30%",
+                        }
+                      : Responsive800
+                      ? {
+                          height: 140,
+                          width: 210,
+                          backgroundPosition: "94% 30%",
+                        }
+                      : Responsive1240
+                      ? {
+                          height: 160,
+                          width: 240,
+                          backgroundPosition: "94% 30%",
+                        }
+                      : {}
+                  }
+                  key={index}
+                />
+              </>
+            );
+          })}
           <Link href="/Search">
-            <Image13 />
+            <Image
+              src={"/./images/Category/search.png"}
+              alt="search tab"
+              height={200}
+              width={288}
+              style={
+                Responsive480
+                  ? {
+                      height: 150,
+                      width: 250,
+                      backgroundPosition: "80% 30%",
+                    }
+                  : Responsive800
+                  ? {
+                      height: 140,
+                      width: 210,
+                      backgroundPosition: "94% 30%",
+                    }
+                  : Responsive1240
+                  ? {
+                      height: 160,
+                      width: 240,
+                      backgroundPosition: "94% 30%",
+                    }
+                  : {}
+              }
+            />
           </Link>
         </ImagesPlace>
       </Container>
