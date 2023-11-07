a7427572a5f100ae2fda4a5c59e4a44f744126cf
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-11-08 01:59:05 +0330

    Change login image

--- app/Styled Components/login.ts
+++ app/Styled Components/login.ts
@@ -9,7 +9,7 @@ export const Container = styled.div`
   align-items: center;
   background-position: 50% 65%;
   background-size: cover;
-  background-image: url("./images/Login/background.svg");
+  background-image: url("./images/Login/background.png");
 `;
 
 export const MiddleContainer = styled.div`
