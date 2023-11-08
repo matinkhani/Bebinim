40f68b524f0d48e2914b738d4ea195d8e3ec57b6
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-11-08 14:01:01 +0330

    Change CinemaOnline images png to webp

--- app/Online-Cinema/onlineMovie.ts
+++ app/Online-Cinema/onlineMovie.ts
@@ -1,7 +1,7 @@
 const OnlineMovie = [
   {
     id: 1,
-    url: "/./images/CinemaOnline/ablagh.png",
+    url: "/./images/CinemaOnline/ablagh.webp",
     name: "ابلق",
     category: "فیلم",
     year: "۱۳۹۹",
@@ -19,7 +19,7 @@ const OnlineMovie = [
   },
   {
     id: 14,
-    url: "/./images/CinemaOnline/worldwar3.png",
+    url: "/./images/CinemaOnline/worldwar3.webp",
     name: "جنگ جهانی سوم",
     category: "فیلم",
     year: "۱۴۰۱",
@@ -37,7 +37,7 @@ const OnlineMovie = [
   },
   {
     id: 15,
-    url: "/./images/CinemaOnline/maslahat.png",
+    url: "/./images/CinemaOnline/maslahat.webp",
     name: "مصلحت",
     category: "فیلم",
     year: "۱۳۹۹",
@@ -55,7 +55,7 @@ const OnlineMovie = [
   },
   {
     id: 16,
-    url: "/./images/CinemaOnline/fosil.png",
+    url: "/./images/CinemaOnline/fosil.webp",
     name: "فسیل",
     category: "فیلم",
     year: "۱۳۹۹",
