0fc773761ab2703bab6466d70a90d2e2cee5cb49
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-11-11 12:54:59 +0330

    Edit catoegory urls

--- app/category/array.ts
+++ app/category/array.ts
@@ -1,72 +1,72 @@
 const CategoryArray = [
   //1
   {
-    src: "/./images/Category/serial.webp",
+    src: "/images/Category/serial.webp",
     alt: "serial tab",
   },
   //2
   {
-    src: "/./images/Category/ekhtesasi.webp",
+    src: "/images/Category/ekhtesasi.webp",
     alt: "exclusive tab",
   },
   //3
   {
-    src: "/./images/Category/online.webp",
+    src: "/images/Category/online.webp",
     alt: "cinema online tab",
   },
   //4
   {
-    src: "/./images/Category/irani.webp",
+    src: "/images/Category/irani.webp",
     alt: "irani tab",
   },
   //5
   {
-    src: "/./images/Category/imdb.webp",
+    src: "/images/Category/imdb.webp",
     alt: "best of imdb tab",
   },
   //6
   {
-    src: "/./images/Category/rielitisho.webp",
+    src: "/images/Category/rielitisho.webp",
     alt: "reality show tab",
   },
   //7
   {
-    src: "/./images/Category/taksho.webp",
+    src: "/images/Category/taksho.webp",
     alt: "talk show tab",
   },
   //8
   {
-    src: "/./images/Category/animat.webp",
+    src: "/images/Category/animat.webp",
     alt: "animation tab",
   },
   //9
   {
-    src: "/./images/Category/khareji.webp",
+    src: "/images/Category/khareji.webp",
     alt: "film tab",
   },
   //10
   {
-    src: "/./images/Category/torki.webp",
+    src: "/images/Category/torki.webp",
     alt: "turkey movie tab",
   },
   //11
   {
-    src: "/./images/Category/koreii.webp",
+    src: "/images/Category/koreii.webp",
     alt: "korean movie tab",
   },
   //12
   {
-    src: "/./images/Category/hendi.webp",
+    src: "/images/Category/hendi.webp",
     alt: "indian movie tab",
   },
   //13
   {
-    src: "/./images/Category/mostanad.webp",
+    src: "/images/Category/mostanad.webp",
     alt: "mostanad tab",
   },
   //14
   {
-    src: "/./images/Category/kotah.webp",
+    src: "/images/Category/kotah.webp",
     alt: "short movie tab",
   },
 ];
